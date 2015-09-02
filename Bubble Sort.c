89cc5be591776e0fa007e52ba6a076369af6bc73
Kawsar Ahmed <mkawsar10@gmail.com>
2015-09-02 19:04:56 +0600

    Bubble Sort

---
+++ Bubble Sort.c
@@ -0,0 +1,26 @@
+/* Bubble Sort */
+
+#include <stdio.h>
+
+int main(){
+
+	int n, i, num[100];
+
+	for(i=0; i<10; i++){
+		scanf("%d", &num[i]);
+	}
+	for(i=0; i<10; i++){
+		for(n=i+1; n<10; n++){
+			if(num[i]> num[n]){
+				int temp = num[i];
+				num[i]= num[n];
+				num[n]= temp;
+			}
+		}
+	}
+	for(i=0; i<10; i++){
+		printf("%d\n", num[i]);
+	}
+
+	return 0;
+}
